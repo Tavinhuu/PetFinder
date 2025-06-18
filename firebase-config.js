e0ae8eca40b5383463b46de7666d125aee005370
Gustavo Henrique <48604457+Tavinhuu@users.noreply.github.com>
2025-06-18 08:59:06 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
-// firebase-config.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
 
-// Configuração do Firebase
+
 const firebaseConfig = {
   apiKey: "",
   authDomain: "",
@@ -14,9 +14,9 @@ const firebaseConfig = {
   measurementId: ""
 };
 
-// Inicializa Firebase
+
 const app = initializeApp(firebaseConfig);
 
-// Exporta instâncias
+
 export const db = getFirestore(app);
 export const auth = getAuth(app);
